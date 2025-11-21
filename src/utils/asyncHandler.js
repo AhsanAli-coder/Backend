2c274afe539493df698c643875353fd1f835ed43
Ahsan Ali <ahsanriaz0475@gmail.com>
2025-11-22 01:54:39 +0500

    Add utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,13 @@
+// asyncHandler.js
+// Wraps async route controllers to automatically catch errors
+// and forward them to Express error middleware.
+
+const asyncHandler = (fn) => {
+  // Return a new function that Express can call with req, res, next
+  return (req, res, next) => {
+    // Call the controller and catch any errors automatically
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
+
+export { asyncHandler };
